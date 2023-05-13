57f0567dd5bee8ad2873a2059bb43519bf84a354
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2023-05-13 16:45:45 +0900

    first commit

---
+++ .storybook/preview.js
@@ -0,0 +1,18 @@
+import { withScreenshot } from "storycap";
+
+/** @type { import('@storybook/react').Preview } */
+const preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+};
+
+export const decorators = [withScreenshot];
+
+export default preview;
